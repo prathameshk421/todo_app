cd7484bfae38ce97eba18c23584bffb7fd3bca6f
prathameshk421 <prathameshkinagi421@gmai.com>
2025-04-22 05:26:26 +0530

    Todo, Createtodo added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {CreateTodo} from './components/CreateTodo'
-import {Todo} from './components/Todo'
+import {CreateTodo} from './components/CreateTask'
+import {Todo} from './components/Task'
 
 function App() {
     return(
